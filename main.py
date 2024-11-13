d265305b5db0168eaed009c4f8b9fa79598a20a5
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-11-13 20:22:47 +0100

    main

--- main.py
+++ main.py
@@ -2,17 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-#funcio GET que retorna un missatge de benvinguda
+#funcio GET que retorna un missatge
 @app.get("/")
-def read_root():
-    return {"message": "Benvingut a la nostra API!"}
-
-#funcio GET que retorna informacio d'un usuari segons el seu id
-@app.get("/user/{user_id}")
-def read_user(user_id: int):
-    return {"user_id": user_id, "name": "Usuari Exemplar"}
-
-#funcio GET que retorna una llista d'items
-@app.get("/items/")
-def read_items():
-    return {"items": ["item1" , "item2", "item3"]}
+async def root():
+    return {"message": "Hello World!!"}
