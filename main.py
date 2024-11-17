76e112cfae777cdac025bb6d464e213891fb8d19
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-11-17 23:30:47 +0100

    afegir error 404 amb response

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, Response
 from typing import Optional
 
+
 app = FastAPI()
 
 data = {"1": "Item 1", "2": "Item 2"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, respose: Response):
-    if item_id not in data_
+async def read_item(item_id: str, response: Response):
+    if item_id not in data:
         response.status_code = 404
         return{"error": "item not found"}
     return {"item_id": item_id, "name": data[item_id]}
