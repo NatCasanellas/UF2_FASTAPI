d93d5c34ed761255804a6a977a23be515ff36f36
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-11-17 23:33:51 +0100

    substituir response per httpException per menjar error 404

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, Response
-from typing import Optional
+from fastapi import FastAPI, HTTPException
 
 
 app = FastAPI()
@@ -8,8 +7,7 @@ data = {"1": "Item 1", "2": "Item 2"}
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: str, response: Response):
+async def read_item(item_id: str):
     if item_id not in data:
-        response.status_code = 404
-        return{"error": "item not found"}
+        raise HTTPException(status_code = 404, detail="Item not found")
     return {"item_id": item_id, "name": data[item_id]}
