23ba99ffdcb2cab1887252f832db69798c52ce04
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-11-17 22:52:05 +0100

    codi metode POST i esquema Base Model

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World!!"}
 
+
+#model POST
+class Item(BaseModel):
+    name: str
+    description: str
+    price: float
+    tax: float
+    category: str
+    in_stock: bool
+
+app = FastAPI()
+
+
+#funcio POST
+@app.post("/items/")
+async def create_item(item: Item):
+    return {"item": item}
