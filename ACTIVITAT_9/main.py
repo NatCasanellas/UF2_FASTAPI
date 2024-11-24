008a93762f5442cef93c9e99879dbe65715addd6
Natalia <2023_natalia.casanellas@iticbcn.cat>
2024-11-24 23:46:18 +0100

    main

--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -19,3 +19,5 @@ class Item(BaseModel):
 async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
+
+#####################################################################
